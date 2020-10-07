2d96385ab50365fb0ef053fc7d14da31fb24deea
Bayramgeldi <bnurberdiyev@colotek.com>
2020-10-07 18:46:07 -0400

    Updates

---
+++ precache-manifest.ac7d1d0ec5f42c937aa17d4cfe907714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36096f0d43023a2e4a7259a7231a5502",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "d7a555e8acb94445170b",
+    "url": "/quote-generator/static/css/main.311a6bff.chunk.css"
+  },
+  {
+    "revision": "607ff2bffa5891eda1f0",
+    "url": "/quote-generator/static/js/2.2646e39d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-generator/static/js/2.2646e39d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a555e8acb94445170b",
+    "url": "/quote-generator/static/js/main.8fae56cc.chunk.js"
+  },
+  {
+    "revision": "6e71cb284c179124fe30",
+    "url": "/quote-generator/static/js/runtime-main.8faf9592.js"
+  }
+]);
